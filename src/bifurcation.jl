88f15b37758cee91886eccc40d27ee4991ada6c9
David AW Barton <david.barton@bristol.ac.uk>
2023-11-16 17:07:56 +0000

    Event detection

--- src/bifurcation.jl
+++ src/bifurcation.jl
@@ -8,7 +8,7 @@ function recordPO(x, p)
             u0 = xtt[:, 1])
 end
 
-function trace_from_equilibrium(p0)
+function trace_from_equilibrium(p0; save_par = nothing)
     # Initial solution
     u0 = zeros(6)
 
@@ -25,12 +25,21 @@ function trace_from_equilibrium(p0)
 
     # Periodic orbit problem
     optn_po = NewtonPar(verbose = false, tol = 1e-8, max_iterations = 25)
+    if save_par !== nothing
+        detect_bifurcation = 0
+        detect_event = 1
+        event = SaveAtEvent((save_par, 0.0))
+    else
+        detect_bifurcation = 3
+        detect_event = 0
+        event = SaveAtEvent((0.0,))
+    end
     opts_po_cont = ContinuationPar(dsmax = 0.1, ds = -0.01, dsmin = 1e-4, p_max = 25.0,
                                    p_min = 0.0, max_steps = 200,
                                    newton_options = (@set optn_po.tol = 1e-8), nev = 3,
-                                   tol_stability = 1e-4, detect_bifurcation = 3,
+                                   tol_stability = 1e-4, detect_bifurcation = detect_bifurcation,
                                    plot_every_step = 20, save_sol_every_step = 1,
-                                   n_inversion = 6)
+                                   n_inversion = 6, detect_event = detect_event)
 
     Mt = 50  # number of time sections
     br_po = continuation(br_eq, 2, opts_po_cont,
@@ -41,6 +50,6 @@ function trace_from_equilibrium(p0)
                          finalise_solution = (z, tau, step, cont_result; prob = nothing, kwargs...) -> begin
                          # try to stop at the Hopf bifurcation to avoid continuing the trivial equilibrium
                          return norm(z.u[begin:(end - 1)]) > 1e-6 end,
-                         normC = norminf, bothside = true)
+                         normC = norminf, bothside = true, event = event)
     return (br_eq, br_po)
 end
